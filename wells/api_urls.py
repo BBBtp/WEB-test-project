53717531900eda862702a6e695dfb67e14b0c4f0
Bogdan <ciborg123@yandex.ru>
2025-11-11 11:32:14 +0300

    fix urls

--- wells/api_urls.py
+++ wells/api_urls.py
@@ -10,20 +10,25 @@ urlpatterns = [
     path('symptoms/<int:pk>/delete/', api_views.ClinicalSymptomDeleteAPIView.as_view(), name='api_symptom_delete'),
     path('symptoms/<int:symptom_id>/add-to-draft/', api_views.add_symptom_to_draft, name='api_add_symptom_to_draft'),
     path('symptoms/<int:symptom_id>/upload-image/', api_views.upload_symptom_image, name='api_upload_symptom_image'),
-    
+
     # Домен заявки
     path('cart/info/', api_views.get_cart_info, name='api_cart_info'),
-    path('assessments/', api_views.RiskAssessmentListAPIView.as_view(), name='api_assessments_list'),
-    path('assessments/<int:pk>/', api_views.RiskAssessmentDetailAPIView.as_view(), name='api_assessment_detail'),
-    path('assessments/<int:pk>/update/', api_views.RiskAssessmentUpdateAPIView.as_view(), name='api_assessment_update'),
-    path('assessments/<int:assessment_id>/form/', api_views.form_assessment, name='api_form_assessment'),
-    path('assessments/<int:assessment_id>/complete/', api_views.complete_assessment, name='api_complete_assessment'),
-    path('assessments/<int:assessment_id>/delete/', api_views.delete_assessment, name='api_delete_assessment'),
-    
+    path('deep-vein-thrombosis/', api_views.RiskAssessmentListAPIView.as_view(), name='api_assessments_list'),
+    path('deep-vein-thrombosis/<int:pk>/', api_views.RiskAssessmentDetailAPIView.as_view(),
+         name='api_assessment_detail'),
+    path('deep-vein-thrombosis/<int:pk>/update/', api_views.RiskAssessmentUpdateAPIView.as_view(),
+         name='api_assessment_update'),
+    path('deep-vein-thrombosis/<int:assessment_id>/form/', api_views.form_assessment, name='api_form_assessment'),
+    path('deep-vein-thrombosis/<int:assessment_id>/complete/', api_views.complete_assessment,
+         name='api_complete_assessment'),
+    path('deep-vein-thrombosis/<int:assessment_id>/delete/', api_views.delete_assessment, name='api_delete_assessment'),
+
     # Домен м-м (симптомы в заявке)
-    path('assessment-symptoms/<int:pk>/', api_views.AssessmentSymptomUpdateAPIView.as_view(), name='api_assessment_symptom_update'),
-    path('assessment-symptoms/<int:pk>/delete/', api_views.AssessmentSymptomDeleteAPIView.as_view(), name='api_assessment_symptom_delete'),
-    
+    path('deep-vein-thrombosis-symptoms/<int:pk>/', api_views.AssessmentSymptomUpdateAPIView.as_view(),
+         name='api_assessment_symptom_update'),
+    path('deep-vein-thrombosis-symptoms/<int:pk>/delete/', api_views.AssessmentSymptomDeleteAPIView.as_view(),
+         name='api_assessment_symptom_delete'),
+
     # Домен пользователь (пока пустые)
     path('users/register/', api_views.user_register, name='api_user_register'),
     path('users/profile/', api_views.user_profile, name='api_user_profile'),
